1e442852d6d6f3a6bbde3e833a6027abd8380f9a
John Barnette <jbarnette@gmail.com>
2008-06-14 23:08:12 -0700

    Remove some outdated bits.

--- lib/basis/errors.rb
+++ lib/basis/errors.rb
@@ -1,15 +1,7 @@
 module Basis
-  class Reportable < StandardError; end
-    
-  class DirectoryNotFound < Reportable
+  class DirectoryNotFound < StandardError
     def initialize(directory)
       super("#{directory} doesn't exist")
-    end
-  end
-
-  class UnknownScheme < Reportable
-    def initialize(url)
-      super("Unknown URL scheme: #{url}")
     end
   end
 end
